615c7596ca40ebc5c982c27a8fb574de83726a17
alexdragnea <alexdg722@gmail.com>
2023-11-16 19:40:29 +0200

    Incremental development

--- src/main/java/com/example/interviewskeleton/config/SecurityConfig.java
+++ src/main/java/com/example/interviewskeleton/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.interviewskeleton.config;
 
 import com.example.interviewskeleton.auth.RequestHeaderAuthenticationProvider;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,14 +35,15 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(requestHeaderAuthenticationFilter(), HeaderWriterFilter.class)
+                .addFilterAfter(requestHeaderAuthenticationFilter(), HeaderWriterFilter.class)
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/**")
+                        auth.requestMatchers(HttpMethod.POST, "/**")
                                 .permitAll()
-                                .requestMatchers("/greet/**")
+                                .requestMatchers(HttpMethod.GET, "/greet/**")
                                 .permitAll()
                                 .anyRequest()
-                                .authenticated()).anonymous(anonymous -> anonymous.disable());
+                                .authenticated()).exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, authException) ->
+                        response.sendError(HttpServletResponse.SC_FORBIDDEN)));
 
         return http.build();
     }
